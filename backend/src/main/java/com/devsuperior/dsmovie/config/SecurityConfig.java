bae5462ee6d335413116a3facdd5dc1c45856513
Michael Dambock <mdambock@gmail.com>
2022-06-04 23:45:45 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.devsu
+package com.devsuperior.dsmovie.config;
 
 import java.util.Arrays;
 
@@ -13,8 +13,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-perior.dsmovie.config;
 
 @Configuration
 @EnableWebSecurity
